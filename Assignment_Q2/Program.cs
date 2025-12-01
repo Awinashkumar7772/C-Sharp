138f6c18443add55430bad241963de8364619d6c
Awinash kumar <kawinash10@gmail.com>
2025-12-01 17:19:14 +0530

    Add files via upload

---
+++ Assignment_Q2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment_Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your height in inches: ");
+            double inches = double.Parse(Console.ReadLine());
+            double centimeters = (inches * 2.54);
+            if (centimeters > 180)
+            {
+                Console.WriteLine("Your are tall.");
+            }
+            else if (centimeters > 150)
+            {
+                Console.WriteLine("Your are of medium height.");
+            }
+            else
+            {
+                Console.WriteLine("Your are not tall...");
+            }
+            
+        }
+    }
+}
