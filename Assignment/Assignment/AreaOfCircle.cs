1c1ee94f2d2adcf229b04fecdd1e24543b3198a5
Awinash kumar <kawinash10@gmail.com>
2025-12-01 17:18:09 +0530

    Add files via upload

---
+++ Assignment/Assignment/AreaOfCircle.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment
+{
+    internal class AreaOfCircle
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radiues of the circle");
+            int radius = Convert.ToInt32(Console.ReadLine());
+            double area = (double)(3.14 * (radius) * radius);
+            Console.WriteLine("Here is the area of circle: " + area);
+
+        }
+    }
+}
