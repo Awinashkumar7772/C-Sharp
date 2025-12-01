9c01d40b946f3cd5c6ce5a3828555952858061c9
Awinash kumar <kawinash10@gmail.com>
2025-12-01 17:20:33 +0530

    Add files via upload

---
+++ Assignment_Q3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int number = int.Parse(Console.ReadLine());
+            int nearest = ((number + 500) / 1000) * 1000;
+            Console.WriteLine("Here is the ans: " + nearest);
+        }
+    }
+}
